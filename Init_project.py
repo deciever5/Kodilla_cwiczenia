14e41083a819bef90e37396bc51e465fd82db3f3
Kunikowski <Marcin.Kunikowski@assaabloy.com>
2022-09-29 11:09:11 +0200

    adding print statements

--- Init_project.py
+++ Init_project.py
@@ -1,8 +1,11 @@
-print ("Lista zakupów")
-shops = {"piekarnia":["chleb","bułki","pączek"], "warzywniak":["marchew", "rukola","seler"]}
+print("Lista zakupów")
+shops = {"piekarnia": ["chleb", "bułki", "pączek"], "warzywniak": ["marchew", "rukola", "seler"]}
 number_of_items = 0
 for shop in shops:
     capital_values = []
-    for i,item in enumerate(shops.get(shop)):
+    for i, item in enumerate(shops.get(shop)):
         capital_values.append(item.capitalize())
-        number_of_items +=1
+        number_of_items += 1
+
+    print(f'Idę do {shop.capitalize()}, kupuję tu następujące rzeczy:{capital_values}')
+print(f"W sumie kupuję {number_of_items} produktów")
