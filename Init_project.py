97300b5e3ed3fb3646df1384e676bbbbb6828fa0
Kunikowski <Marcin.Kunikowski@assaabloy.com>
2022-09-29 11:03:38 +0200

    adding main loop

--- Init_project.py
+++ Init_project.py
@@ -1,2 +1,8 @@
 print ("Lista zakupów")
 shops = {"piekarnia":["chleb","bułki","pączek"], "warzywniak":["marchew", "rukola","seler"]}
+number_of_items = 0
+for shop in shops:
+    capital_values = []
+    for i,item in enumerate(shops.get(shop)):
+        capital_values.append(item.capitalize())
+        number_of_items +=1
