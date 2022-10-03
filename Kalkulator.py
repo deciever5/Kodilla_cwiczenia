4becdf786e1ef3895fd97b658cc518de8a6ec094
Kunikowski <Marcin.Kunikowski@assaabloy.com>
2022-10-03 11:29:23 +0200

    added value check

--- Kalkulator.py
+++ Kalkulator.py
@@ -2,11 +2,6 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-choice = input \
-    ("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
-num1 = input("Podaj składnik 1: ")
-num2 = input("Podaj składnik 2: ")
-
 
 def do_the_math(x, a, b):
     if x == "1":
@@ -16,13 +11,24 @@ def do_the_math(x, a, b):
         logging.debug(f" Odejmuję:{num1},{num2}")
         return a - b
     elif x == "3":
-        logging.debug(f" Mnożę:{num1},{num2}")
+        logging.debug(f" Mnożę: {num1} , {num2}")
         return a * b
     elif x == "4":
         logging.debug(f" Dzielę:{num1},{num2}")
         return a / b
+
+
+while True:
+    choice = input(
+        "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
+    if choice in ["1", "2", "3", "4"]:
+        try:
+            num1 = float(input("Podaj składnik 1: "))
+            num2 = float(input("Podaj składnik 2: "))
+            print(f"Wynik to: {do_the_math(choice, num1, num2)}")
+            break
+        except ValueError:
+            print("Podaj tylko liczby!")
+
     else:
-        print("Niewłaściwy wybór!")
-
-
-print(f"Wynik to: {do_the_math(choice, num1, num2)}")
+        print("Nieprawidłowy wybór. Wybierz działanie (1-4)")
