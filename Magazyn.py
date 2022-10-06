f372b1140ce51bf39ea3a1976e3699b0b00fdb0e
deciever5 <marcinkunikowski@gmail.com>
2022-10-06 14:00:00 +0200

    adding add sell item, get costs in progress

--- Magazyn.py
+++ Magazyn.py
@@ -1,11 +1,16 @@
-items = {"milk": [100, "l", 2.5], "coffee": [50, "kg", 10], "tea": [30, "kg", 5], "pepsi": [200, "l", 6],
+import copy
+
+goods = {"milk": [100, "l", 2.5], "coffee": [50, "kg", 10], "tea": [30, "kg", 5], "pepsi": [200, "l", 6],
          "wine": [10, "l", 30], "beer": [200, "l", 3]}
+
+sold_goods = copy.deepcopy(goods)
+for k, v in sold_goods.items(): v[0] = 0
 
 
 def getitems():
     print("Name\tQuantity\tUnit\tUnit Price (PLN)")
     print("----\t--------\t----\t----------------")
-    for key, value in items.items():
+    for key, value in goods.items():
         print(f"{key:8}{repr(value[0]).ljust(12)}{value[1].ljust(8)}{repr(value[2]).ljust(16)}")
 
 
@@ -14,7 +19,19 @@ def add_item():
     unit_name = input("Item units of measure :")
     quantity = input("Item quantity: ")
     unit_price = input("Unit price in PLN: ")
-    items[name] = [int(quantity), unit_name, int(unit_price)]
+    goods[name] = [int(quantity), unit_name, int(unit_price)]
+
+
+def sell_item():
+    sold_item = input("Item sold: ")
+    quantity_sold = input("Quantity sold: ")
+    goods[sold_item][0] -= int(quantity_sold)
+    print(f"Succesfully sold {quantity_sold}{goods[sold_item][1]} of {sold_item}")
+    sold_goods[sold_item][0] += int(quantity_sold)
+    getitems()
+
+def get_costs():
+    pass
 
 
 while True:
@@ -25,3 +42,5 @@ while True:
         getitems()
     elif user_operation == "add":
         add_item()
+    elif user_operation == "sell":
+        sell_item()
