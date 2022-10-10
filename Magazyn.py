d84f8c2161554fc22abab15d558313aa3717b1a5
deciever5 <marcinkunikowski@gmail.com>
2022-10-10 07:38:55 +0200

    optimizing functions

--- Magazyn.py
+++ Magazyn.py
@@ -1,8 +1,6 @@
 import copy
 import csv
 import sys
-import tkinter
-from tkinter import *
 
 goods = {"milk": [100.5, "l", 2.5], "coffee": [50, "kg", 10], "tea": [30, "kg", 5], "pepsi": [200, "l", 6],
          "wine": [10, "l", 30], "beer": [200, "l", 3]}
@@ -11,38 +9,24 @@ sold_goods = copy.deepcopy(goods)
 for k, v in sold_goods.items(): v[0] = 0
 
 
-def export_csv():
+def export_csv(filename, data):
     headers = ["Name", "Quantity", "Unit", "Unit Price (PLN)"]
-    with open("goods.csv", "w", newline="") as csvfile:
+    with open(filename, "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(headers)
-        writer.writerows([key, *value] for key, value in goods.items())
-    export_sales_to_csv()
+        writer.writerows([key, *value] for key, value in data)
 
 
-def export_sales_to_csv():
-    headers = ["Name", "Quantity", "Unit", "Unit Price (PLN)"]
-    with open("sold_goods.csv", "w", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(headers)
-        writer.writerows([key, *value] for key, value in sold_goods.items())
-
-
-def load_items_from_csv():
-    with open(sys.argv[1], newline="") as csvfile:
+def load_items_from_csv(argument):
+    with open(argument, newline="") as csvfile:
         for _, value in goods.items(): value.clear()
         reader = csv.reader(csvfile)
         next(reader)
         for row in reader:
             goods[row[0]] = [float(row[1]), row[2], float(row[3])]
-    with open(sys.argv[2], newline="") as csvfile:
-        for _, value in sold_goods.items(): value.clear()
-        reader = csv.reader(csvfile)
-        next(reader)
-        for row in reader:
-            sold_goods[row[0]] = [float(row[1]), row[2], float(row[3])]
 
-    print(f"Successfully loaded data from {sys.argv[1]} and {sys.argv[2]}")
+    print(f"Successfully loaded data from {sys.argv[1]}")
+    print(f"Successfully loaded data from {sys.argv[2]}")
 
 
 def get_items():
@@ -85,39 +69,11 @@ def show_revenue():
 
 
 if len(sys.argv) > 1:
-    load_items_from_csv()
+    load_items_from_csv(sys.argv[1])
+    load_items_from_csv(sys.argv[2])
 
 
-class App:
-    def __init__(self, master):
-        fm1 = Frame(master)
-        fm2 = Frame(master)
-        fm3 = Frame(master)
-        exiting = tkinter.Button(fm3, text="Exit", command=exit)
-        exiting.pack(side="right")
-        show = tkinter.Button(fm2, text="Show warehouse", command=get_items)
-        show.pack(side="top")
-        add = tkinter.Button(fm1, text="Add item", command=add_item)
-        add.pack(side="left")
-        sell = tkinter.Button(fm1, text="Sell item", command=sell_item)
-        sell.pack(side="left")
-        cost = tkinter.Button(fm2, text="Warehouse value", command=get_costs)
-        cost.pack(side="top")
-        revenue = tkinter.Button(fm2, text="Revenue value", command=show_revenue)
-        revenue.pack(side="top")
-        save = tkinter.Button(fm3, text="Save warehouse", command=export_csv)
-        save.pack(side="right")
-        fm1.pack(side=LEFT, fill=BOTH, expand=YES)
-        fm2.pack(side=LEFT, padx=10)
-        fm3.pack(side=RIGHT, padx=10)
 
-
-window = tkinter.Tk()
-window.title("Storage managment")
-display = App(window)
-window.mainloop()
-
-"""
 while True:
     user_operation = input("What would you like to do? ")
     if user_operation == "exit":
@@ -128,13 +84,11 @@ while True:
         add_item()
     elif user_operation == "sell":
         sell_item()
-    elif user_operation == "cost":
-        get_costs()
     elif user_operation == "revenue":
         show_revenue()
     elif user_operation == "save":
-        export_csv()
-        export_sales_to_csv()
+        export_csv("goods.csv", goods.items())
+        export_csv("sold_goods.csv", sold_goods.items())
 
     else:
-        print("No such operation.")"""
+        print("No such operation.")
