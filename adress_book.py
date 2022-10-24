77277684f867dbbb1c2cd8bf1f99c487f47ac078
deciever5 <marcinkunikowski@gmail.com>
2022-10-24 09:07:26 +0200

    introducing subclasses

--- adress_book.py
+++ adress_book.py
@@ -2,27 +2,44 @@ from faker import Faker
 
 fake = Faker()
 
-random_plp = [(fake.name(), fake.address(), fake.email(), fake.company(), fake.job()) for _ in range(5)]
 
-
-class BusinessCard:
+class BaseContact:
     def __init__(self):
         self.name = fake.name()
         self.address = fake.address()
         self.email = fake.email()
-        self.company = fake.company()
-        self.job = fake.job()
+        self.home_phone = fake.phone_number()
+        self._name_len = len(self.name)
+
+    def contact(self):
+        print(f'Wybieram numer telefonu {self.home_phone} i dzwonię do {self.name}')
 
     def __str__(self):
-        return f'{self.name}, {self.address}, {self.email}, {self.company}, {self.job}'
+        return f'{self.name}, {self.address}, {self.email}, {self.home_phone}'
+
+    def label_length(self):
+        print(len(self.name))
+        return len(self.name)
 
 
-wizytowka1 = BusinessCard()
-wizytowka2 = BusinessCard()
-wizytowka3 = BusinessCard()
-wizytowka4 = BusinessCard()
-wizytowka5 = BusinessCard()
-lista_wizytowek = [wizytowka1, wizytowka2, wizytowka3, wizytowka4, wizytowka5]
-by_name = (sorted(lista_wizytowek, key=lambda card: card.email))
-for i in range(len(by_name)):
-    print(by_name[i])
+class BusinessContact(BaseContact):
+    def __init__(self):
+        super().__init__()
+        self.company = fake.company()
+        self.job = fake.job()
+        self.work_phone = fake.phone_number()
+
+    def __str__(self):
+        return f'{self.name}, {self.company}, {self.email}, {self.work_phone}'
+
+    def contact(self):
+        print(f'Wybieram numer {self.work_phone} i dzwonię do {self.name}')
+
+
+
+
+lista_wizytowek = [BaseContact() for _ in range(5)]
+lista_wizytowek2 = [BusinessContact() for _ in range(5)]
+lista_wizytowek[0].contact()
+lista_wizytowek2[0].contact()
+print(lista_wizytowek2[0].label_length())
