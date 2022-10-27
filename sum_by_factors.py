fd2a57dca1c5134cdc0bee5118b4dc722f3d7407
deciever5 <marcinkunikowski@gmail.com>
2022-10-27 12:01:48 +0200

    added main function - kata sum factors

--- sum_by_factors.py
+++ sum_by_factors.py
@@ -1,5 +1,5 @@
 def sum_for_list(lst):
-    primes_in_range = []
+    primes_in_range, result, previous_prime = [], [], 0
     for num in range(2, lst[-1]):
         if num > 1:
             for i in range(2, num):
@@ -7,7 +7,15 @@ def sum_for_list(lst):
                     break
             else:
                 primes_in_range.append(num)
-    print(primes_in_range)
 
-lst = [15, 21, 24, 30, 45]
-sum_for_list(lst)
+    all_poss = ([[prime, num] for prime in primes_in_range for num in lst if num % prime == 0])
+    for i, j in enumerate(all_poss):
+        if not result:
+            result.append(j)
+        elif previous_prime != j[0]:
+            result.append(j)
+        else:
+            result[-1][1] += j[1]
+        previous_prime = j[0]
+
+    return result
