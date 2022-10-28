8713e6c885df5173c7f245931cd799916ee47037
deciever5 <marcinkunikowski@gmail.com>
2022-10-28 10:02:50 +0200

    optimizing speed

--- sum_by_factors.py
+++ sum_by_factors.py
@@ -1,5 +1,5 @@
 import math
-import sys
+
 
 def is_prime(n):
     if n <= 1:
@@ -10,14 +10,22 @@ def is_prime(n):
             return False
     return True
 
-def sum_for_list(lst_to_check):
-    primes_in_range, result, previous_prime = [], [], 0
-    primes_in_range  = [num for num in range(2, max([abs(num) for num in lst_to_check]) + 1) if is_prime(num)]
-    #all_poss = [[prime, num] for prime in primes_in_range for num in lst_to_check if num % prime == 0]
-    all_poss2 = ([prime, num] for prime in primes_in_range for num in lst_to_check if num % prime == 0)
-    print(sys.getsizeof(all_poss2), type(all_poss2))
 
-    for i, j in all_poss2:
+def my_generator(lst_to_check):
+    primes_in_range = [num for num in range(2, max([abs(num) for num in lst_to_check]) + 1) if is_prime(num)]
+    all_possibilities = ([prime, num] for prime in primes_in_range for num in lst_to_check if num % prime == 0)
+    all_possibilities2 = map(lambda x: [x[0],x[1]]if  x[1] % x[0] == 0 and x[1] != 0 else None,
+                             ([prime, num] for prime in primes_in_range for num in lst_to_check if num % prime == 0))
+    for j in all_possibilities2:
+        if j is not None:
+            yield j
+
+
+def sum_for_list(lst_to_check):
+    result, previous_prime = [], 0
+    all_results = my_generator(lst_to_check)
+
+    for j in all_results:
         if not result:
             result.append(j)
         elif previous_prime != j[0]:
@@ -25,20 +33,21 @@ def sum_for_list(lst_to_check):
         else:
             result[-1][1] += j[1]
         previous_prime = j[0]
-
-
     return result
 
-def profiling(lst):
+
+def profiling(lst_to_time):
     import cProfile
     import pstats
 
     with cProfile.Profile() as pr:
-        sum_for_list(lst)
+        sum_for_list(lst_to_time)
     stats = pstats.Stats(pr)
     stats.sort_stats(pstats.SortKey.TIME)
     stats.print_stats()
     stats.dump_stats(filename="obliczenia.prof")
 
+
 lst = [x for x in range(30000)]
 profiling(lst)
+# sum_for_list(lst)
