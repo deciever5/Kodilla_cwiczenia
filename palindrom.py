286825815257f6f7fdc8a4ccfa1ec8b6e21adbc9
Marcin Kunikowski <marcinkunikowski@gmail.com>
2022-11-04 19:51:23 +0100

    sorting project files properly

--- palindrom.py
+++
@@ -1,7 +0,0 @@
-def palindrome_check(x):
-    """Sprawdzenie łańcucha znaków pod kątem bycia palindromem """
-    return x[::] == x[::-1]
-
-
-word = input("Podaj słowo do sprawdzenia: ")
-print(f'Słowo {word} to{[" nie ", " "][palindrome_check(word)]}palindrom')
