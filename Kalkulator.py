231699e3bea1d4346a2e1a7dbac21e2aee73265f
Marcin Kunikowski <marcinkunikowski@gmail.com>
2023-01-30 21:35:07 +0100

    TDD training - adding function exist test

--- Kalkulator.py
+++ Kalkulator.py
@@ -1,34 +1,10 @@
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-
-
-def do_the_math(x, a, b):
-    if x == "1":
-        logging.debug(f" Dodaję:{num1},{num2}")
-        return a + b
-    elif x == "2":
-        logging.debug(f" Odejmuję:{num1},{num2}")
-        return a - b
-    elif x == "3":
-        logging.debug(f" Mnożę: {num1} , {num2}")
-        return a * b
-    elif x == "4":
-        logging.debug(f" Dzielę:{num1},{num2}")
-        return a / b
-
-
-while True:
-    choice = input(
-        "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
-    if choice in ["1", "2", "3", "4"]:
-        try:
-            num1 = float(input("Podaj składnik 1: "))
-            num2 = float(input("Podaj składnik 2: "))
-            print(f"Wynik to: {do_the_math(choice, num1, num2)}")
-            break
-        except ValueError:
-            print("Podaj tylko liczby!")
-
-    else:
-        print("Nieprawidłowy wybór. Wybierz działanie (1-4)")
+def test_prime_factors(self):
+    # Test if prime number is returned as a single-element list
+    self.assertEqual(prime_factors(13), [13])
+    # Test if prime factors are returned in a list
+    self.assertEqual(prime_factors(159172), [2, 2, 13, 3061])
+    # Test if input is 0 or 1
+    self.assertEqual(prime_factors(0), [])
+    self.assertEqual(prime_factors(1), [])
+    # Test if input is negative number
+    self.assertEqual(prime_factors(-159172), [2, 2, 13, 3061])
--- prime_factors/prime_factors.py
+++ prime_factors/prime_factors.py
@@ -1,26 +1,15 @@
 import unittest
 
 
-def prime_factors(number):
-    factors = []
-    for i in range(2, number + 1):
-        while number % i == 0:
-            factors.append(i)
-            number = number / i
-    return factors
+def prime_factors():
+    pass
+
 
 
 class PrimeFactorsTest(unittest.TestCase):
-    def test_prime_factors(self):
-        # Test if prime number is returned as a single-element list
-        self.assertEqual(prime_factors(13), [13])
-        # Test if prime factors are returned in a list
-        self.assertEqual(prime_factors(159172), [2, 2, 13, 3061])
-        # Test if input is 0 or 1
-        self.assertEqual(prime_factors(0), [])
-        self.assertEqual(prime_factors(1), [])
-        # Test if input is negative number
-        self.assertEqual(prime_factors(-159172), [2, 2, 13, 3061])
+    def test_function_exist(self):
+        self.assertTrue(callable(prime_factors))
+
 
 
 if __name__ == '__main__':
