24b49391309194cdc40e03fd73d21a75e57062bc
Marcin Kunikowski <marcinkunikowski@gmail.com>
2023-01-30 22:19:32 +0100

    TDD training - testing input and output

--- prime_factors/prime_factors.py
+++ prime_factors/prime_factors.py
@@ -1,15 +1,19 @@
 import unittest
 
 
-def prime_factors():
-    pass
-
+def prime_factors(number):
+    return []
 
 
 class PrimeFactorsTest(unittest.TestCase):
     def test_function_exist(self):
         self.assertTrue(callable(prime_factors))
 
+    def test_input_exist(self):
+        self.assertRaises(TypeError, prime_factors(6))
+
+    def test_return_as_list(self):
+        self.assertEqual(type(prime_factors(6)),list)
 
 
 if __name__ == '__main__':
